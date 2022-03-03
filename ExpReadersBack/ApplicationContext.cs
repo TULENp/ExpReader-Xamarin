52fdf8e946fa54cca6be553dc84b4fca5052c66a
Arkadashim <andrewsha.antonov@mail.ru>
2022-03-03 20:56:11 +0300

    Added backend

---
+++ ExpReadersBack/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using ExpReader.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpReadersBack
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
+        public DbSet<Book> Books { get; set; }
+    }
+}
