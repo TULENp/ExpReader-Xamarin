8db39fb95035cd1536100f3223b448f890ef8111
Arkadashim <andrewsha.antonov@mail.ru>
2022-03-04 14:30:33 +0300

    Added User

---
+++ DAL/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Nickname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int ReadBooks { get; set; }
+        public int ReadPoints { get; set; }
+    }
+}
