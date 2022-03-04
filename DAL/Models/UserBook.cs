b50bd50ae12d33de1dd64c8bd539b2ed320686e9
Arkadashim <andrewsha.antonov@mail.ru>
2022-03-04 23:23:30 +0300

    userbook model added

---
+++ DAL/Models/UserBook.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+    class UserBook
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Key]
+        public int BookID { get; set; }
+    }
+}
