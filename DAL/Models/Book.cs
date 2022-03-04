564498c31aef397efe01cd197ea6b4f790cacc39
Arkadashim <andrewsha.antonov@mail.ru>
2022-03-04 14:30:45 +0300

    Added Book

---
+++ DAL/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+    class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
