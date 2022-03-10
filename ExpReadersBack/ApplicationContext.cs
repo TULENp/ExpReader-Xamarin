9a3240d5ed8db89187a6c9a9ea5c17e8e1d9615a
Arkadashim <andrewsha.antonov@mail.ru>
2022-03-10 18:53:51 +0300

    Changed for db

--- ExpReadersBack/ApplicationContext.cs
+++ ExpReadersBack/ApplicationContext.cs
@@ -1,11 +1,22 @@
-﻿using ExpReader.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using DAL.Models;
 
 namespace ExpReadersBack
 {
     public class ApplicationContext : DbContext
     {
-        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
         public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserBook> UserBooks { get; set; }
+        public DbSet<UserStats> UserStatistics { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserBook>().HasKey(ub => new { ub.BookId, ub.UserId });
+        }
     }
 }
