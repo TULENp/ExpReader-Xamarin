b4cf7cb3f79aba2959ea3ecc2afb0d1ea3a66e77
kildanL <97839751+kildanL@users.noreply.github.com>
2022-03-14 20:30:59 +0300

    Added slide-out settings panel in ReaderPage

--- ExpReader/Views/ReaderPage.xaml.cs
+++ ExpReader/Views/ReaderPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ExpReader.Models;
 using ExpReader.ViewModels;
 using System.IO;
+using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -22,6 +23,47 @@ namespace ExpReader.Views
         private void ScrollToTop(object sender, System.EventArgs e)
         {
             scroll.ScrollToAsync(0, 0, false);
+        }
+
+        private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            HideSettingsPanel();
+        }
+
+        private async void HideSettingsPanel()
+        {
+            SettingsPanel.TranslationY = -80;
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
+        {
+            SettingsPanel.TranslateTo(0, 0, 250, Easing.CubicInOut);
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, System.EventArgs e)
+        {
+            //SettingsPanel.TranslateTo(0, -80, 250, Easing.CubicInOut);
+            if((imageBack.IsVisible==true) && (imageGear.IsVisible==true))
+            {
+                imageBack.TranslateTo(0, -80, 250, Easing.CubicIn);
+                imageGear.TranslateTo(0, -80, 250, Easing.CubicIn);
+                Task.Delay(250);
+                imageBack.IsVisible = false;
+                Task.Delay(250);
+                imageGear.IsVisible = false;
+            }
+            else
+            {
+                imageBack.TranslateTo(0, 0, 250, Easing.CubicInOut);
+                imageBack.IsVisible = true;
+                imageGear.TranslateTo(0, 0, 250, Easing.CubicInOut);
+                imageGear.IsVisible = true;
+            }
         }
     }
 }
