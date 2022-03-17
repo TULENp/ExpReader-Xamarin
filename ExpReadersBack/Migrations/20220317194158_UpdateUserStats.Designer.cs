2019ebe6e8356890e606229d1bdba5a50e6502c7
Arkadashim <andrewsha.antonov@mail.ru>
2022-03-18 02:24:23 +0300

    Add migrations, update userstats in db.

--- ExpReadersBack/Migrations/20220317194158_UpdateUserStats.Designer.cs
+++ ExpReadersBack/Migrations/20220317194158_UpdateUserStats.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ExpReadersBack;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ExpReadersBack.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220317194158_UpdateUserStats")]
+    partial class UpdateUserStats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
