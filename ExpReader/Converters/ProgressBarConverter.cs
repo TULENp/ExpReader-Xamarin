63871a72fd0054553307ebbb1df1ec64d7ef8230
Arkadashim <andrewsha.antonov@mail.ru>
2022-03-19 04:12:50 +0300

    ProgressBatConverter Added.

---
+++ ExpReader/Converters/ProgressBarConverter.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ExpReader.Converters
+{
+    class ProgressBarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var book = (Book)value;
+            try
+            {
+                return book.CurrentPage / book.Pages;
+            } catch
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
